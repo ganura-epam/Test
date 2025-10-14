4f6c2486995c3631e95a13ba6b9f0df9d5b0d5ac
“Anurag <anurag_gupta2@epam.com>
2025-10-14 15:13:57 +0530

    Comminting new changes

---
+++ src/com/App.java
@@ -0,0 +1,12 @@
+package com;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("2 + 3 = " + add(2, 3));
+    }
+
+    // Adds two integers and returns the sum
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
