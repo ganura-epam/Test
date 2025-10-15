8967862643140740183e8e2613960952e8f109e1
“Anurag <anurag_gupta2@epam.com>
2025-10-15 08:59:08 +0530

    fixed the code

--- src/com/App.java
+++ src/com/App.java
@@ -3,7 +3,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("2 + 3 = " + add(2, 3));
-        System.out.println("5 + 7 = " + add2(5, 7));
+        System.out.println("5 + 7 = " + add(5, 7));
     }
 
     // Adds two integers and returns the sum
