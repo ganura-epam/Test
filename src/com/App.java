1dbbe48b63b44a2c7e2f2db720051ed13112fb28
“Anurag <anurag_gupta2@epam.com>
2025-10-17 14:45:12 +0530

    code change

--- src/com/App.java
+++ src/com/App.java
@@ -3,7 +3,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("2 + 3 = " + add(2, 3));
-        System.out.println("5 + 7 = " + add1(5, 7));
     }
 
     // Adds two integers and returns the sum
