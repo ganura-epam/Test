f8c61707c3e97b2e66a70e787701b5fdfa69fc79
“Anurag <anurag_gupta2@epam.com>
2025-10-17 18:51:03 +0530

    build break

--- src/com/App.java
+++ src/com/App.java
@@ -3,7 +3,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("2 + 3 = " + add(2, 3));
-         System.out.println("2 + 3 = " + add1(2, 3));
+        System.out.println("2 + 3 = " + add(2, 3));
+        System.out.println("2 + 3 = " + add123(2, 3));
     }
 
     // Adds two integers and returns the sum
