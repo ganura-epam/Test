4cb958cb5cd7e0b3dc425118d270834decb230b4
“Anurag <anurag_gupta2@epam.com>
2025-10-17 14:46:42 +0530

    make build fail

--- src/com/App.java
+++ src/com/App.java
@@ -3,6 +3,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("2 + 3 = " + add(2, 3));
+         System.out.println("2 + 3 = " + add1(2, 3));
     }
 
     // Adds two integers and returns the sum
