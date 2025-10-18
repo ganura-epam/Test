670188af9652342ffcf42c55d958347b6ca6d466
“Anurag <anurag_gupta2@epam.com>
2025-10-18 13:23:56 +0530

    Added subtract function and updated main method

--- src/com/App.java
+++ src/com/App.java
@@ -3,12 +3,17 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("2 + 3 = " + add(2, 3));
-        System.out.println("2 + 3 = " + add(2, 3));
-        System.out.println("2 + 3 = " + add(7, 3));
+        System.out.println("7 + 3 = " + add(7, 3));
+        System.out.println("7 - 3 = " + subtract(7, 3));
     }
 
     // Adds two integers and returns the sum
     public static int add(int a, int b) {
         return a + b;
+    }
+
+    // Subtracts second integer from first and returns the difference
+    public static int subtract(int a, int b) {
+        return a - b;
     }
 }
