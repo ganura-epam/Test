845fe25a226d2fa3f84c9dc361292a48dffc78a2
ganura-epam <anurag_gupta2@epam.com>
2025-11-07 16:45:06 +0530

    Add overloaded add method for float parameters

--- src/com/App.java
+++ src/com/App.java
@@ -4,12 +4,17 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("2 + 3 = " + add(2, 3));
         System.out.println("7 + 3 = " + add(7, 3));
-
+        System.out.println("7.5 + 3 = " + add(7.5f, 3f));
     
     }
 
     // Adds two integers and returns the sum
     public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Adds two floats and returns the sum
+    public static float add(float a, float b) {
         return a + b;
     }
 
